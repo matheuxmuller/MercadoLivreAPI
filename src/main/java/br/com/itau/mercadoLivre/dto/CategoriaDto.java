cd5ec44b0abfb4b9ecc2c30693449a0228d3ea59
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 13:43:33 -0300

    Create CategoriaDto.java

---
+++ src/main/java/br/com/itau/mercadoLivre/dto/CategoriaDto.java
@@ -0,0 +1,70 @@
+package br.com.itau.mercadoLivre.dto;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.itau.mercadolivre.dto.ProdutoDto;
+import br.com.itau.mercadolivre.form.ImagensForm;
+import br.com.itau.mercadolivre.form.ProdutoForm;
+import br.com.itau.mercadolivre.form.Uploader;
+import br.com.itau.mercadolivre.model.Produto;
+import br.com.itau.mercadolivre.model.Usuario;
+import br.com.itau.mercadolivre.repository.CategoriaRepository;
+import br.com.itau.mercadolivre.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private Uploader uploader;
+
+	@PostMapping
+	public void PostProduto(@RequestBody @Valid ProdutoForm produtoForm, @AuthenticationPrincipal Usuario usuario) {
+		Produto produto = produtoForm.converter(categoriaRepository, usuario);
+		produtoRepository.save(produto);
+	}
+
+	@GetMapping
+	public List<ProdutoDto> GetAll() {
+		List<Produto> produtos = produtoRepository.findAll();
+		return ProdutoDto.converter(produtos);
+	}
+
+	@PostMapping("/{id}/imagens")
+	public void AddImagens(@PathVariable Long id, @Valid ImagensForm imagensForm,
+			@AuthenticationPrincipal Usuario usuario) {
+		List<String> links = uploader.envia(imagensForm.getImagens());
+
+		Optional<Produto> produto = produtoRepository.findById(id);
+
+		if (produto.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O produto não existe no sistema!");
+		}
+
+		if (produto.get().getUsuario().getId() != usuario.getId()) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "A imagem só pode ser cadastrada pelo seu usuário original!");
+		}
+		produto.get().associaImagens(links);
+		produtoRepository.save(produto.get());
+	}
+}
