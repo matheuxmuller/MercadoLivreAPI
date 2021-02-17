42019ea9e99996f50e392dcbe1cbec389f6fab6b
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 15:59:34 -0300

    Create CompraDto.java

---
+++ src/main/java/br/com/itau/mercadoLivre/dto/CompraDto.java
@@ -0,0 +1,57 @@
+package br.com.itau.mercadoLivre.dto;
+
+import java.math.BigDecimal;
+
+import br.com.zup.mercadolivre.enums.GatewayPagamento;
+import br.com.zup.mercadolivre.enums.StatusCompra;
+import br.com.zup.mercadolivre.model.Compra;
+
+public class CompraDto {
+	
+	private Long id;
+	private String nomeProduto;
+	private BigDecimal valorProduto;
+	private Long quantidade;
+	private StatusCompra statusCompra;
+	private GatewayPagamento gateway;
+	private String urlPagamento;
+	
+	public CompraDto(Compra compra, String urlPagamento) {
+		this.id = compra.getId();
+		this.quantidade = compra.getQuantidade();
+		this.nomeProduto = compra.getProduto().getNome();
+		this.valorProduto = compra.getProduto().getValor();
+		this.statusCompra = compra.getStatusCompra();
+		this.gateway = compra.getGateway();
+		this.urlPagamento = urlPagamento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public BigDecimal getValorProduto() {
+		return valorProduto;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public StatusCompra getStatusCompra() {
+		return statusCompra;
+	}
+
+	public GatewayPagamento getGateway() {
+		return gateway;
+	}
+
+	public String getUrlPagamento() {
+		return urlPagamento;
+	}
+	
+}
