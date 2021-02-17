e319284662d4bdc179f10e3278fd135fa56b3a12
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 13:44:24 -0300

    Create ProdutoDto.java

---
+++ src/main/java/br/com/itau/mercadoLivre/dto/ProdutoDto.java
@@ -0,0 +1,65 @@
+package br.com.itau.mercadoLivre.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import br.com.itau.mercadolivre.model.Caracteristica;
+import br.com.itau.mercadolivre.model.Produto;
+
+public class ProdutoDto {
+
+	private String nome;
+	private BigDecimal valor;
+	private Long quantidade;
+	@JsonIgnoreProperties(value = "produto")
+	private List<Caracteristica> caracteristicas;
+	private String descricao;
+	private String categoria;
+	private LocalDateTime cadastradoEm = LocalDateTime.now();
+
+	public ProdutoDto(Produto produto) {
+		this.nome = produto.getNome();
+		this.valor = produto.getValor();
+		this.quantidade = produto.getQuantidade();
+		this.caracteristicas = produto.getCaracteristicas();
+		this.descricao = produto.getDescricao();
+		this.categoria = produto.getCategoria().getNome();
+		this.cadastradoEm = produto.getCadastradoEm();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public List<Caracteristica> getCaracteristicas() {
+		return caracteristicas;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public LocalDateTime getCadastradoEm() {
+		return cadastradoEm;
+	}
+
+	public static List<ProdutoDto> converter(List<Produto> produtos) {
+		return produtos.stream().map(ProdutoDto::new).collect(Collectors.toList());
+	}
+}
