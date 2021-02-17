9476821ef7cb0e4a5d11933d9b4a0f184c9e95fe
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 16:19:21 -0300

    Create Emails.java

---
+++ src/main/java/br/com/itau/mercadoLivre/model/Emails.java
@@ -0,0 +1,20 @@
+package br.com.itau.mercadoLivre.model;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.itau.mercadoLivre.repository.Mailer;
+
+@Service
+public class Emails {
+ 
+	@Autowired
+	private Mailer mailer;
+	
+	public void novaPergunta(@NotNull @Valid Pergunta pergunta) {
+		mailer.send("<html>...</html>", "Nova pergunta...", pergunta.getUsuario().getLogin(),"novapergunta@nossomercadolivre.com", pergunta.getProduto().getUsuario().getLogin());
+	}
+}
