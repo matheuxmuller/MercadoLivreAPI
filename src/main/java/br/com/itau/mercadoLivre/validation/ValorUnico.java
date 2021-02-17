b7e82149506a49f8cb9d193c986d6444a8159bfa
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 14:05:15 -0300

    Create ValorUnico.java

---
+++ src/main/java/br/com/itau/mercadoLivre/validation/ValorUnico.java
@@ -0,0 +1,26 @@
+package br.com.itau.mercadoLivre.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+
+@Documented
+@Constraint(validatedBy = {ValorUnicoValidator.class})
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface ValorUnico {
+
+    String message() default "{}";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+    String fieldName();
+    Class<?> domainClass();
+	
+}
+
