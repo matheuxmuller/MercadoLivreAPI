e79f2ffe983c60b114f8c10e57be36b5e1139741
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 14:26:23 -0300

    Update ProdutoRepository.java

--- src/main/java/br/com/itau/mercadoLivre/repository/ProdutoRepository.java
+++ src/main/java/br/com/itau/mercadoLivre/repository/ProdutoRepository.java
@@ -1,8 +1,8 @@
 package br.com.itau.mercadoLivre.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import br.com.itau.mercadoLivre.model.Produto;
 
 @Repository
