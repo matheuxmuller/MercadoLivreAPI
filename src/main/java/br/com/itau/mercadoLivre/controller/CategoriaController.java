14ab3958b29b3c292aca7cfb51b4f2af09524337
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 13:35:07 -0300

    Update CategoriaController.java

--- src/main/java/br/com/itau/mercadoLivre/controller/CategoriaController.java
+++ src/main/java/br/com/itau/mercadoLivre/controller/CategoriaController.java
@@ -5,34 +5,33 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.itau.mercadoLivre.model.Categoria;
-import br.com.itau.mercadoLivre.repository.CategoriaRepository;
-import br.com.itau.mercadoLivre.request.CategoriaRequest;
-import br.com.itau.mercadoLivre.request.MetodosConversao;
-
+import br.com.itau.mercadolivre.dto.CategoriaDto;
+import br.com.itau.mercadolivre.form.CategoriaForm;
+import br.com.itau.mercadolivre.model.Categoria;
+import br.com.itau.mercadolivre.repository.CategoriaRepository;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
-
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+		
+	@PostMapping
+	public void PostCategoria(@RequestBody @Valid CategoriaForm categoriaForm) {
+		Categoria categoria = categoriaForm.converter(categoriaRepository);
+		categoriaRepository.save(categoria);
+	}
 	
 	@GetMapping
-	public ResponseEntity<List<Categoria>> GetAll (){
-		return ResponseEntity.ok(categoriaRepository.findAll());
+	public List<CategoriaDto> GetAll() {
+		List<Categoria> categorias = categoriaRepository.findAll();
+		return CategoriaDto.converter(categorias);
 	}
-
-	@PostMapping
-	public ResponseEntity<?> cadastrar(@RequestBody @Valid CategoriaRequest categoriaRequest) {
-		Categoria categoria = MetodosConversao.ConverteRequestParaCategoria(categoriaRequest);
-		categoriaRepository.save(categoria);
-		return ResponseEntity.ok().build();
-	}
+		
 }
