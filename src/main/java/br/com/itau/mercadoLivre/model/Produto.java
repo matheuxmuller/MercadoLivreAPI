2b2553cac2a16abdd6cd8fb98b9b46f2c2c07fa5
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 13:55:52 -0300

    Update Produto.java

--- src/main/java/br/com/itau/mercadoLivre/model/Produto.java
+++ src/main/java/br/com/itau/mercadoLivre/model/Produto.java
@@ -1,67 +1,128 @@
 package br.com.itau.mercadoLivre.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_produto")
 public class Produto {
-	
-	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-	private @OneToMany List<Caracteristicas> caracteristicas;
+
+	/**
+	 * ESSA TA OK *
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Column(nullable = false)
+	private String nome;
+
+	@NotNull
+	@Column(nullable = false)
+	private BigDecimal valor;
+
+	@NotNull
+	@Column(nullable = false)
 	private Long quantidade;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "produto_id")
+	private List<Caracteristica> caracteristicas = new ArrayList<Caracteristica>();
+
+	@NotBlank
+	@Column(nullable = false, length = 1000)
 	private String descricao;
-	private String categoria;
-	private LocalDateTime instanteCadastro;
-	private Long idUsuario;
-	
-	@Deprecated
+
+	@NotNull
+	@OneToOne(cascade = CascadeType.ALL)
+	private Categoria categoria;
+
+	@ManyToOne(cascade = CascadeType.REFRESH)
+	private Usuario usuario;
+
+	private LocalDateTime cadastradoEm = LocalDateTime.now();
+
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.MERGE)
+	private List<ImagemProduto> imagens = new ArrayList<ImagemProduto>();
+
 	public Produto() {
 	}
 
-	public Produto(List<Caracteristicas> caracteristicas, Long quantidade, String descricao, String categoria,
-			LocalDateTime instanteCadastro, Long idUsuario) {
-		super();
-		this.caracteristicas = caracteristicas;
+	public Produto(@NotBlank String nome, @NotNull BigDecimal valor, @NotNull Long quantidade,
+			List<Caracteristica> caracteristicas, @NotBlank String descricao, @NotNull Categoria categoria,
+			Usuario usuario) {
+		this.nome = nome;
+		this.valor = valor;
 		this.quantidade = quantidade;
+		this.caracteristicas = caracteristicas;
 		this.descricao = descricao;
 		this.categoria = categoria;
-		this.instanteCadastro = instanteCadastro;
-		this.idUsuario = idUsuario;
+		this.usuario = usuario;
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-	public List<Caracteristicas> getCaracteristicas() {
-		return caracteristicas;
+	public String getNome() {
+		return nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
 	}
 
 	public Long getQuantidade() {
 		return quantidade;
 	}
 
+	public List<Caracteristica> getCaracteristicas() {
+		return caracteristicas;
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
 
-	public String getCategoria() {
+	public Categoria getCategoria() {
 		return categoria;
 	}
 
-	public LocalDateTime getInstanteCadastro() {
-		return instanteCadastro;
+	public Usuario getUsuario() {
+		return usuario;
 	}
 
-	public Long getIdUsuario() {
-		return idUsuario;
+	public LocalDateTime getCadastradoEm() {
+		return cadastradoEm;
+	}
+
+	public void associaImagens(List<String> links) {
+		List<ImagemProduto> imagens = links.stream().map(link -> new ImagemProduto(this, link))
+				.collect(Collectors.toList());
+		this.imagens.addAll(imagens);
+	}
+
+	public <T> List<T> mapImagens(Function<ImagemProduto, T> funcaoMap) {
+		return this.imagens.stream().map(funcaoMap).collect(Collectors.toList());
 	}
 }
