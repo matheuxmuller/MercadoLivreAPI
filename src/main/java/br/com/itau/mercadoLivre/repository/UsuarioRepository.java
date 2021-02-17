810188ae9ecadf676db2676b59ce27e9005f7448
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 13:59:38 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/itau/mercadoLivre/repository/UsuarioRepository.java
+++ src/main/java/br/com/itau/mercadoLivre/repository/UsuarioRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.itau.mercadoLivre.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	Optional<Usuario> findByUsuario(String usuario);
+	Optional<Usuario> findByLogin(String username);
 }
