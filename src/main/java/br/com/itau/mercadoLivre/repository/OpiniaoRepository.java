80dfd1db296c111310a6e21709932fbeb1fd8934
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 16:11:22 -0300

    Create OpiniaoRepository.java

---
+++ src/main/java/br/com/itau/mercadoLivre/repository/OpiniaoRepository.java
@@ -0,0 +1,8 @@
+package br.com.itau.mercadoLivre.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.itau.mercadoLivre.model.Opiniao;
+
+public interface OpiniaoRepository extends JpaRepository<Opiniao, Long>{
+
+}
