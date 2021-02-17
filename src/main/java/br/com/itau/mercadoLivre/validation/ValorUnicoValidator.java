88226aae8a7c07f8424edbb4c40733b901a8b674
Matheus Müller <67790499+matheuxmuller@users.noreply.github.com>
2021-02-17 14:04:52 -0300

    Create ValorUnicoValidator.java

---
+++ src/main/java/br/com/itau/mercadoLivre/validation/ValorUnicoValidator.java
@@ -0,0 +1,37 @@
+package br.com.itau.mercadoLivre.validation;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.util.Assert;
+
+public class ValorUnicoValidator implements ConstraintValidator<ValorUnico, Object> {
+
+	private String atributoAPesquisar;
+	private Class<?> classeEntidade;
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public void initialize(ValorUnico params) {
+		atributoAPesquisar = params.fieldName();
+		classeEntidade = params.domainClass();
+	}
+
+	@Override
+	public boolean isValid(Object valorAPesquisar, ConstraintValidatorContext context) {
+		Query query = manager
+				.createQuery("select 1 from " + classeEntidade.getName() + " where " + atributoAPesquisar + "=:value");
+		query.setParameter("value", valorAPesquisar);
+		List<?> list = query.getResultList();
+		Assert.state(list.size() <= 1, "Foi encontrado mais de um " + classeEntidade + "com o atributo "
+				+ atributoAPesquisar + " = " + valorAPesquisar);
+
+		return list.isEmpty();
+	}
+}
